8d88f69a957c60c56dd1c80ae3f6e8a36ca31a2c
developerkawsar <flutter.kawsar@gmail.com>
2021-07-03 22:37:30 +0600

    Day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:product_delivery_app/pages/home_page.dart';
+import 'package:product_delivery_app/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.red),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
 }
